8f4f66349f1443e0a391c2df35604cef77b71e4c
aakumar07 <126548776+aakumar07@users.noreply.github.com>
2023-12-10 22:26:03 +0530

    Create variable.tf

---
+++ aws/ec2-instance/variable.tf
@@ -0,0 +1,26 @@
+variable "vpc_cidr" {
+    description = "configure the cidr"
+    type = string
+}
+
+variable "awspubsub_cidr" {
+    description = "configure the cidr"
+    type = string
+}
+
+variable "awsprisub_cidr" {
+    description = "configure the cidr"
+    type = string
+}
+
+variable "igw_cidr" {
+    description = "configure the cidr"
+    type = string
+}
+
+variable "ami_id" {
+  type = string
+}
+variable "instace_type_id"
+  type = string
+}
